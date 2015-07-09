62b20230423d8a546d0ec0eeb97ed0ed51408f55
nicklawson <nlgit@element4.net>
2015-07-09 11:18:26 +0100

    first commit

---
+++ src/main/java/sprBoot/SprBootApplication.java
@@ -0,0 +1,12 @@
+package sprBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprBootApplication.class, args);
+    }
+}
---
+++ src/test/java/sprBootEureka/SprBootEurekaApplicationTests.java
@@ -0,0 +1,18 @@
+package sprBootEureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import sprBoot.SprBootApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SprBootApplication.class)
+public class SprBootEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
